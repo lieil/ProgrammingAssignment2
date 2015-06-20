d8768ea1131d03c04e89effce7fd553138bce314
Elena Frolova <elicfr@mail.ru>
2015-06-20 20:13:37 +0300

    course2 programming assignment 2. makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Function "makeCacheMatrix" create "special matrix object", 
+## where stored data of original matrix and its inversed matrix (where it will be counting),
+## and function cacheSolve retun inverse matrix from saved data, or counting it at first time
 
-## Write a short comment describing this function
+
+## 1) Function create "special matrix object" from the input matrix, 
+## and return list of function to get-set matrix and get-set inversed matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+     set <- function(y) {
+         x <<- y
+         m <<- NULL
+     }
+     get <- function() x
+     setsolve <- function(solve) m <<- solve
+     getsolve <- function() m
+     list(set = set, get = get,
+          setsolve = setsolve,
+          getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## 2) Function or calculates inversed matrix of "special matrix object",  
+## or return it from saved data if it already exist
 
 cacheSolve <- function(x, ...) {
+  m <- x$getsolve()
+     if(!is.null(m)) {
+         message("getting cached data")
+         return(m)
+     }
+     data <- x$get()
+     m <- solve(data, ...)
+     x$setsolve(m)
+     m
         ## Return a matrix that is the inverse of 'x'
 }
